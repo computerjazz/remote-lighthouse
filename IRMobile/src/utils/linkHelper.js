fd35ac1815023f38972b82a64762db9d9c724639
Daniel Merrill <daniel.r.merrill@gmail.com>
2017-09-27 23:49:24 -0700

    prevent duplicate branch links

--- IRMobile/src/utils/linkHelper.js
+++ IRMobile/src/utils/linkHelper.js
@@ -4,9 +4,19 @@ import { importRemote, setEditMode } from '../actions'
 // Set ttl to 0 to prevent duplicates
 branch.initSessionTtl = 0
 
+let previousTimestamp = 0
+
 export default dispatch => {
   branch.subscribe(bundle => {
+    console.log('GOT A LINK!', bundle)
     if (bundle && bundle.params && !bundle.error) {
+      const timestamp = bundle.params['+click_timestamp']
+      console.log('TIMESTAMP', timestamp)
+      if (timestamp === previousTimestamp) {
+        console.log('duplicate, skipping')
+        return
+      }
+      previousTimestamp = timestamp
       // grab deep link data and route appropriately.
       if (bundle.params.remote) {
         // Fix branch issue on android that double-escapes strings
